dcbb27dfbabfa3bd1da13313552ca21e51a80e29
John M Davis Jr <jmdcodejunkie@gmail.com>
2016-09-11 02:19:28 -0700

    getting scrolling texture setted up...
    lloks great, just need the images to be set to texture... et al

---
+++ Assets/Scripts/SceneScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SceneScroller : MonoBehaviour {
+    public float scrollSpeed;
+    private MeshRenderer textureRenderer;
+    void Start ()
+    {
+        textureRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+    void Update ()
+    {
+        // float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        // transform.position = startPosition + Vector3.forward * newPosition;
+
+        float x = Mathf.Repeat(Time.time * this.scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, 0);
+
+        textureRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+    }
+}
